a6ff169401874d3ca09e049c0ece0a5b5da3290c
typhonrt <support@typhonjs.io>
2023-06-30 19:13:23 -0700

    update build

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,15 @@ import dts           from 'rollup-plugin-dts';
 // Produce sourcemaps or not.
 const sourcemap = true;
 
-const external = ['./generator.js', 'svelte/internal', 'svelte/store'];
+// Marks all local package.json `imports` and `svelte` as external.
+const external = [/#simple-web-storage\/*/, /svelte\/*/];
+
+// Converts local package.json imports to final package sub-path exports.
+const paths = {
+   '#simple-web-storage/generator': '@typhonjs-svelte/simple-web-storage/generator',
+   '#simple-web-storage/local': '@typhonjs-svelte/simple-web-storage/local',
+   '#simple-web-storage/session': '@typhonjs-svelte/simple-web-storage/session'
+};
 
 /**
  * @returns {import('rollup').RollupOptions[]}
@@ -13,53 +21,74 @@ export default () =>
 {
    return [
       // Main Distribution -------------------------------------------------------------------------------------------
+
       {
-         input: 'src/generator.ts',
+         input: 'src/index.ts',
          external,
          output: [{
-            file: './dist/generator.js',
+            file: './dist/index.js',
             format: 'es',
             generatedCode: { constBindings: true },
-            sourcemap,
+            paths,
+            sourcemap
          }],
          plugins: [
-            typescript({ include: ['src/generator.ts'] })
+            typescript({ include: ['src/index.ts'] })
          ]
       },
       {
-         input: 'src/local.ts',
+         input: 'src/generator/index.ts',
          external,
          output: [{
-            file: './dist/local.js',
+            file: './dist/generator/index.js',
             format: 'es',
             generatedCode: { constBindings: true },
+            paths,
             sourcemap,
          }],
          plugins: [
-            typescript({ include: ['src/local.ts'] })
+            typescript({ include: ['src/generator/index.ts'] })
          ]
       },
       {
-         input: 'src/session.ts',
+         input: 'src/local/index.ts',
          external,
          output: [{
-            file: './dist/session.js',
+            file: './dist/local/index.js',
             format: 'es',
             generatedCode: { constBindings: true },
+            paths,
             sourcemap,
          }],
          plugins: [
-            typescript({ include: ['src/session.ts'] })
+            typescript({ include: ['src/local/index.ts'] })
+         ]
+      },
+      {
+         input: 'src/session/index.ts',
+         external,
+         output: [{
+            file: './dist/session/index.js',
+            format: 'es',
+            generatedCode: { constBindings: true },
+            paths,
+            sourcemap,
+         }],
+         plugins: [
+            typescript({ include: ['src/session/index.ts'] })
          ]
       },
 
       // Main Distribution Bundled TS Declarations -------------------------------------------------------------------
+
       {
-         input: 'src/generator.ts',
+         input: 'src/index.ts',
+         external,
          output: [{
-            file: `./types/generator.d.ts`,
+            file: `./dist/index.d.ts`,
             format: 'es',
             generatedCode: { constBindings: true },
+            paths,
             sourcemap: false
          }],
          plugins: [
@@ -67,11 +96,41 @@ export default () =>
          ]
       },
       {
-         input: 'src/local.ts',
+         input: 'src/generator/index.ts',
+         external,
          output: [{
-            file: `./types/stores.d.ts`,
+            file: `./dist/generator/index.d.ts`,
             format: 'es',
             generatedCode: { constBindings: true },
+            paths,
+            sourcemap: false
+         }],
+         plugins: [
+            dts()
+         ]
+      },
+      {
+         input: 'src/local/index.ts',
+         external,
+         output: [{
+            file: `./dist/local/index.d.ts`,
+            format: 'es',
+            generatedCode: { constBindings: true },
+            paths,
+            sourcemap: false
+         }],
+         plugins: [
+            dts({ compilerOptions: { removeComments: false }})
+         ]
+      },
+      {
+         input: 'src/session/index.ts',
+         external,
+         output: [{
+            file: `./dist/session/index.d.ts`,
+            format: 'es',
+            generatedCode: { constBindings: true },
+            paths,
             sourcemap: false
          }],
          plugins: [
